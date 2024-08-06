8929b459965cb278d2cf8bfe468d39055b659e1b
code-walker-23 <chhabravinay549@gmail.com>
2024-08-06 16:52:11 +0530

    Project Setup

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute top-0 left-0 right-0 px-8 py-4 bg-transparent z-20 flex items-center">
+      <img
+        className="logo"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
